fc5c11d0b73ee73d6de9d0fb61384df0460c5577
すえ <souhait.0614@gmail.com>
2022-12-30 07:55:20 +0900

    🚧 開発準備完了

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+const projectRoot = __dirname
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(projectRoot, "src"),
+  build: {
+    outDir: path.resolve(projectRoot, "dist")
+  },
   plugins: [react()],
 })
