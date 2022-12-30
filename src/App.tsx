de28c66ff425a4b9855e46a6f04d6482d655f9b5
すえ <souhait.0614@gmail.com>
2022-12-30 20:50:15 +0900

    🚧 とりあえずChakraとRecoilの動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Box, Button } from "@chakra-ui/react"
+import { useRecoilState } from "recoil"
+import { countAtom } from "./atom"
+
 export const App = () => {
+  const [count, setCount] = useRecoilState(countAtom)
   return (
-    <div>App</div>
+    <Box onClick={()=>setCount((prev)=>prev+1)}>
+      <Button>{count}</Button>
+    </Box>
   )
 }
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RecoilRoot } from "recoil"
+import { App } from "./App"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+)
